77907b17f61c5d40549d1494ab797cce45e4130d
Muhammad Ubaid <muhammadubaid202006@gmail.com>
2026-01-13 22:35:07 +0500

    Circumference and area calculator of circle

---
+++ Circle.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Circle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double rad;
+        double area;
+        double cir;
+
+        System.out.print("Enter Your Circle radius: ");
+        rad = input.nextDouble();
+
+        area = 3.14 * rad * rad;
+        cir = 2 * 3.14 * rad;
+
+        System.out.println("Area = " + area);
+        System.out.println("Circumference = " + cir);
+
+        input.close();
+    }
+}
